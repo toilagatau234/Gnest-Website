be6fc2c61932c199fae71a96bf09e2d92425335c
toilagatau234 <toilagatau234@gmail.com>
2026-06-08 13:55:20 +0700

    fix: remove unused React import in RecruitmentJobsContainer

--- components/RecruitmentJobsContainer.tsx
+++ components/RecruitmentJobsContainer.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React, { useState } from 'react';
+import { useState } from 'react';
 import { Briefcase, MapPin, DollarSign, ArrowLeft } from 'lucide-react';
 import { RecruitmentClientActions } from '@/components/RecruitmentClientActions';
 
